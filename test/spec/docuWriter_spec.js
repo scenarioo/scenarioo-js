82c75f8e9f85504d1808e1fc8c21a0c1abb6b51d
Sergio Trentini <xeronimus@gmail.com>
2014-10-23 09:48:34 +0200

    #3 switch to mocha and expect.js for unit testing.

--- test/spec/docuWriter_spec.js
+++ test/spec/docuWriter_spec.js
@@ -4,80 +4,102 @@ var docuWriter = require('../../lib/scenarioDocuWriter.js');
 var extend = require('extend');
 var fs = require('fs');
 
+before(function () {
+
+  // define our mock jasmine object
+  global.jasmine = {
+    getEnv: function () {
+      var env = {
+        currentSpec: {
+          suite: {
+            description: 'SuiteDescription'
+          },
+          description: 'specDescription'
+        }
+      };
+      return env;
+    }
+  };
+});
+
 describe('scenarioDocuWriter', function () {
 
-    /** let's set up some dummy objects **/
-    var targetDir = './test/out/docu';
+  /** let's set up some dummy objects **/
+  var targetDir = './test/out/docu';
 
-    var dummyBranch = {
-        name: 'my unsafe branch name, will',
-        description: 'my safe description'
-    };
+  var dummyBranch = {
+    name: 'my unsafe branch name, will',
+    description: 'my safe description'
+  };
 
-    var dummyBuild = {
-        name: 'some build name_',
-        revision: '1234'
-    };
+  var dummyBuild = {
+    name: 'some build name_',
+    revision: '1234'
+  };
 
-    var dummyUseCase = {
-        name: 'use case name, toll!',
-        description: 'some description with special chars ;) %&'
-    };
+  var dummyUseCase = {
+    name: 'use case name, toll!',
+    description: 'some description with special chars ;) %&'
+  };
 
-    var dummyScenario = {
-        useCaseName: dummyUseCase.name,
-        scenarioName: ' some cool scenario name',
-        scenarioDescription: 'scenario description',
-        stepCounter: 0
-    };
+  var dummyScenario = {
+    useCaseName: dummyUseCase.name,
+    scenarioName: ' some cool scenario name',
+    scenarioDescription: 'scenario description',
+    stepCounter: 0
+  };
 
-    function getTS() {
-        return  Math.round((new Date()).getTime() / 1000);
-    }
+  function getTS() {
+    return  Math.round((new Date()).getTime() / 1000);
+  }
 
-    function getTimeStampedBuildObject(timeStamp) {
-        var build = {};
-        extend(build, dummyBuild);
-        build.name = build.name + timeStamp;
-        return build;
-    }
+  function getTimeStampedBuildObject(timeStamp) {
+    var build = {};
+    extend(build, dummyBuild);
+    build.name = build.name + timeStamp;
+    return build;
+  }
 
-    function assertFileWillExistWithin(filePath, timeout) {
-        waitsFor(function () {
-            return fs.existsSync(filePath);
-        }, filePath + ' written', timeout);
-    }
+  function assertFileExists(filePath, done) {
 
-    it('should write branch on start()', function () {
-        var timeStamp = getTS();
-        docuWriter.start(dummyBranch, getTimeStampedBuildObject(timeStamp), targetDir);
-        var expectedFilePath = targetDir + '/my_unsafe_branch_name__will/branch.xml';
-        assertFileWillExistWithin(expectedFilePath, 2000);
+    fs.exists(filePath, function (err, resu) {
+      done();
     });
+//        waitsFor(function () {
+//            return fs.existsSync(filePath);
+//        }, filePath + ' written', timeout);
+  }
 
-    it('should save usecase', function () {
-        var timeStamp = getTS();
-        docuWriter.start(dummyBranch, getTimeStampedBuildObject(timeStamp), targetDir);
-        docuWriter.saveUseCase(dummyUseCase);
-        var expectedFilePath = targetDir + '/my_unsafe_branch_name__will/some_build_name_' + timeStamp + '/use_case_name__toll_/usecase.xml';
-        assertFileWillExistWithin(expectedFilePath, 2000);
-    });
+  it('should write branch on start()', function (done) {
+    var timeStamp = getTS();
+    docuWriter.start(dummyBranch, getTimeStampedBuildObject(timeStamp), targetDir);
+    var expectedFilePath = targetDir + '/my_unsafe_branch_name__will/branch.xml';
+    assertFileExists(expectedFilePath, done);
+  });
 
-    it('should save scenario', function () {
-        var timeStamp = getTS();
-        docuWriter.start(dummyBranch, getTimeStampedBuildObject(timeStamp), targetDir);
-        docuWriter.saveUseCase(dummyUseCase);
-        docuWriter.saveScenario(dummyScenario);
-        var expectedFilePath = targetDir + '/my_unsafe_branch_name__will/some_build_name_' + timeStamp + '/use_case_name__toll_/_some_cool_scenario_name/scenario.xml';
-        assertFileWillExistWithin(expectedFilePath, 2000);
-    });
+  it('should save usecase', function (done) {
+    var timeStamp = getTS();
+    docuWriter.start(dummyBranch, getTimeStampedBuildObject(timeStamp), targetDir);
+    docuWriter.saveUseCase(dummyUseCase);
+    var expectedFilePath = targetDir + '/my_unsafe_branch_name__will/some_build_name_' + timeStamp + '/use_case_name__toll_/usecase.xml';
+    assertFileExists(expectedFilePath, done);
+  });
 
-    it('should save a step', function() {
-        var timeStamp = getTS();
-        docuWriter.start(dummyBranch, getTimeStampedBuildObject(timeStamp), targetDir);
-        docuWriter.saveStep('my step');
-        var expectedFilePath = targetDir + '/my_unsafe_branch_name__will/some_build_name_' + timeStamp + '/scenariodocuwriter/should_save_a_step/steps/000.xml';
-        assertFileWillExistWithin(expectedFilePath, 2000);
-    });
+  it('should save scenario', function (done) {
+    var timeStamp = getTS();
+    docuWriter.start(dummyBranch, getTimeStampedBuildObject(timeStamp), targetDir);
+    docuWriter.saveUseCase(dummyUseCase);
+    docuWriter.saveScenario(dummyScenario);
+    var expectedFilePath = targetDir + '/my_unsafe_branch_name__will/some_build_name_' + timeStamp + '/use_case_name__toll_/_some_cool_scenario_name/scenario.xml';
+    assertFileExists(expectedFilePath, done);
+  });
+
+  it('should save a step', function (done) {
+    var timeStamp = getTS();
+    docuWriter.start(dummyBranch, getTimeStampedBuildObject(timeStamp), targetDir);
+    docuWriter.saveStep('my step');
+    var expectedFilePath = targetDir + '/my_unsafe_branch_name__will/some_build_name_' + timeStamp + '/scenariodocuwriter/should_save_a_step/steps/000.xml';
+    assertFileExists(expectedFilePath, done);
+  });
 
 });
--- test/spec/util_spec.js
+++ test/spec/util_spec.js
@@ -1,108 +1,109 @@
 'use strict';
 
 var util = require('../../lib/util.js');
+var expect = require('expect.js');
 
 describe('util: isDefined', function () {
 
-    it('should return true for a string', function () {
-        var result = util.isDefined('one two three');
-        expect(result).toBe(true);
-    });
+  it('should return true for a string', function () {
+    var result = util.isDefined('one two three');
+    expect(result).to.be(true);
+  });
 
-    it('should return true for an object', function () {
-        var result = util.isDefined({'my': 'attribute'});
-        expect(result).toBe(true);
-    });
+  it('should return true for an object', function () {
+    var result = util.isDefined({'my': 'attribute'});
+    expect(result).to.be(true);
+  });
 
-    it('should return true for null', function () {
-        var result = util.isDefined(null);
-        expect(result).toBe(true);
-    });
+  it('should return true for null', function () {
+    var result = util.isDefined(null);
+    expect(result).to.be(true);
+  });
 
-    it('should return false for undefined (inline)', function () {
-        var result = util.isDefined(undefined);
-        expect(result).toBe(false);
-    });
+  it('should return false for undefined (inline)', function () {
+    var result = util.isDefined(undefined);
+    expect(result).to.be(false);
+  });
 
-    it('should return false for undefined', function () {
-        var input;
-        var result = util.isDefined(input);
-        expect(result).toBe(false);
-    });
+  it('should return false for undefined', function () {
+    var input;
+    var result = util.isDefined(input);
+    expect(result).to.be(false);
+  });
 });
 
 describe('util: leadingZeros', function () {
 
-    it('should append two zeros (int)', function () {
-        var result = util.leadingZeros(1);
-        expect(result).toBe('001');
-    });
+  it('should append two zeros (int)', function () {
+    var result = util.leadingZeros(1);
+    expect(result).to.be('001');
+  });
 
-    it('should append two zeros', function () {
-        var result = util.leadingZeros('1');
-        expect(result).toBe('001');
-    });
+  it('should append two zeros', function () {
+    var result = util.leadingZeros('1');
+    expect(result).to.be('001');
+  });
 
-    it('should append one zeros', function () {
-        var result = util.leadingZeros('12');
-        expect(result).toBe('012');
-    });
+  it('should append one zeros', function () {
+    var result = util.leadingZeros('12');
+    expect(result).to.be('012');
+  });
 
-    it('should append no zero', function () {
-        var result = util.leadingZeros('123');
-        expect(result).toBe('123');
-    });
+  it('should append no zero', function () {
+    var result = util.leadingZeros('123');
+    expect(result).to.be('123');
+  });
 
 });
 
 describe('util: safeForFilename', function () {
 
-    it('should not escape numbers', function () {
-        var result = util.getSafeForFileName('123');
-        expect(result).toBe('123');
-    });
+  it('should not escape numbers', function () {
+    var result = util.getSafeForFileName('123');
+    expect(result).to.be('123');
+  });
 
-    it('should not escape alphabetic chars', function () {
-        var result = util.getSafeForFileName('abc');
-        expect(result).toBe('abc');
-    });
+  it('should not escape alphabetic chars', function () {
+    var result = util.getSafeForFileName('abc');
+    expect(result).to.be('abc');
+  });
 
-    it('should escape blanks', function () {
-        var result = util.getSafeForFileName('a b c');
-        expect(result).toBe('a_b_c');
-    });
+  it('should escape blanks', function () {
+    var result = util.getSafeForFileName('a b c');
+    expect(result).to.be('a_b_c');
+  });
 
-    it('should escape blanks (alphanumeric)', function () {
-        var result = util.getSafeForFileName('a1 b2 c3');
-        expect(result).toBe('a1_b2_c3');
-    });
+  it('should escape blanks (alphanumeric)', function () {
+    var result = util.getSafeForFileName('a1 b2 c3');
+    expect(result).to.be('a1_b2_c3');
+  });
 
-    it('should escape special chars', function () {
-        var result = util.getSafeForFileName('a+a%,a some');
-        expect(result).toBe('a_a__a_some');
-    });
+  it('should escape special chars', function () {
+    var result = util.getSafeForFileName('a+a%,a some');
+    expect(result).to.be('a_a__a_some');
+  });
 
 });
 
-describe('util: sanitizeIdentifier', function() {
+describe('util: sanitizeIdentifier', function () {
 
-    it('should return the original value if it is not a string', function() {
-        expect(util.sanitizeIdentifier(123)).toBe(123);
-        expect(util.sanitizeIdentifier(null)).toBe(null);
-        expect(util.sanitizeIdentifier(undefined)).toBe(undefined);
-    });
+  it('should return the original value if it is not a string', function () {
+    expect(util.sanitizeIdentifier(123)).to.be(123);
+    expect(util.sanitizeIdentifier(null)).to.be(null);
+    expect(util.sanitizeIdentifier(undefined)).to.be(undefined);
+  });
 
-    it('should return the original value if the string input does not contain forbidden characters', function() {
-        var validString = 'abc123DEF_- .:';
-        expect(util.sanitizeIdentifier(validString)).toBe(validString);
-    });
+  it('should return the original value if the string input does not contain forbidden characters', function () {
+    var validString = 'abc123DEF_- .:';
+    expect(util.sanitizeIdentifier(validString)).to.be(validString);
+  });
 
-    it('should replace slashes', function() {
-        expect(util.sanitizeIdentifier('/some/string')).toBe('_some_string');
-    });
+  it('should replace slashes', function () {
+    expect(util.sanitizeIdentifier('/some/string')).to.be('_some_string');
+  });
 
-    it('should replace backslashes', function() {
-        expect(util.sanitizeIdentifier('\\some\\string')).toBe('_some_string');
-    });
+  it('should replace backslashes', function () {
+    expect(util.sanitizeIdentifier('\\some\\string')).to.be('_some_string');
+  });
 
 });
