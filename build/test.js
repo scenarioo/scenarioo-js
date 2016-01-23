82abf6e4db960ef9621ee91cb9bc21789eb4584a
Sergio Trentini <xeronimus@gmail.com>
2016-01-23 09:29:51 +0100

    [noissue] updating del

--- build/test.js
+++ build/test.js
@@ -3,14 +3,11 @@ var
     mocha = require('gulp-mocha'),
     del = require('del');
 
-    
 gulp.task('test', ['cleanTestOut'], function () {
     return gulp.src('./test/unit/*.js', {read: false})
         .pipe(mocha({reporter: 'spec'}));
 });
 
-gulp.task('cleanTestOut', function (done) {
-    del('test/out/*', function (err) {
-        done(err);
-    });
+gulp.task('cleanTestOut', function () {
+    return del('test/out/*');
 });
