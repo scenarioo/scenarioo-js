b9503d7cf6fd8f264bc68b9e2e333516784f1bf5
Sergio Trentini <xeronimus@gmail.com>
2016-01-23 10:10:50 +0100

    [noissue] remove unused method -> remove "findit" dependency

--- test/utils/testHelper.js
+++ test/utils/testHelper.js
@@ -4,34 +4,7 @@ var
     assert = require('assert'),
     xml2js = require('xml2js'),
     Q = require('q'),
-    fs = require('fs'),
-    findit = require('findit');
-
-
-function logDirectoryTree(rootDir) {
-
-    var deferred = Q.defer();
-
-    var find = findit(rootDir);
-
-    find.on('file', function (file) {
-        console.log(file);
-    });
-
-    find.on('directory', function (file) {
-        console.log(file);
-    });
-
-    find.on('end', function () {
-        deferred.resolve();
-    });
-
-    find.on('error', function () {
-        deferred.reject();
-    });
-
-    return deferred.promise;
-}
+    fs = require('fs');
 
 function assertXmlContent(filePath, content, done) {
     fs.readFile(filePath, function (err, data) {
@@ -58,7 +31,6 @@ function assertFileExists(filePath) {
 }
 
 module.exports = {
-    logDirectoryTree: logDirectoryTree,
     assertXmlContent: assertXmlContent,
     assertFileExists: assertFileExists
 };
