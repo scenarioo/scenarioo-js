4e8eb4b97d49b00df7505160e9f216b73db3ba1a
Rolf Bruderer <bruderer@computerscience.ch>
2016-10-08 21:53:06 +0200

    #33 handle disabled tests: - do not report disabled scenarios - count disabled scenarios to try to identify disabled usecases (=suites) because those are also triggering the jasmine reporter now somehow, have no clue why they once changed that, very annoying - do not report use cases with only disabled scenarios - report use cases without any scenarios allways as SUCCESS

--- src/scenariooReporter.js
+++ src/scenariooReporter.js
@@ -8,6 +8,7 @@ import process from 'process';
 const SUCCESS = 'success';
 const FAILED = 'failed';
 const PENDING = 'pending';
+const DISABLED = 'disabled';
 
 /**
  * This is the base reporter that pulls information from the store and the calling (framework-dependant) reporter
@@ -98,6 +99,7 @@ function useCaseStarted(options, useCaseName) {
     passedScenarios: 0,
     failedScenarios: 0,
     pendingScenarios: 0,
+    disabledScenarios: 0,
     name: useCaseName
   });
 }
@@ -107,12 +109,14 @@ function useCaseStarted(options, useCaseName) {
  * @param {object} options
  */
 function useCaseEnded(options) {
+
   const useCase = store.getCurrentUseCase();
   const build = store.getBuild();
 
   const hasFailingScenarios = useCase.failedScenarios > 0;
   const hasPassedScenarios = useCase.passedScenarios > 0;
   const hasPendingScenarios = useCase.pendingScenarios > 0;
+  const hasDisabledScenarios = useCase.disabledScenarios > 0;
   let useCaseStatus;
   if (hasFailingScenarios) {
     store.updateBuild({
@@ -129,6 +133,18 @@ function useCaseEnded(options) {
       pendingUseCases: build.pendingUseCases + 1
     });
     useCaseStatus = PENDING;
+  }
+  else if (hasDisabledScenarios) {
+    // only disabled scenarios ---> do not report whole use case (consider it as disabled as a whole)
+    return;
+  }
+  else {
+    // Use case without any scenarios is considered an empty scenario with status SUCCESS.
+    // (probably we can not distinguish between disabled and empty use case here, therefore we just report it as SUCCESS)
+    store.updateBuild({
+      passedUseCases: build.passedUseCases + 1
+    });
+    useCaseStatus = SUCCESS;
   }
 
   if (!options.disableScenariooLogOutput) {
@@ -175,6 +191,7 @@ function expectationFailed(options, failureMessage) {
  * @param {string} status one of {@link scenariooReporter#SUCCESS}, {@link scenariooReporter#FAILED}, {@link scenariooReporter#PENDING}
  */
 function scenarioEnded(options, status) {
+
   const scenario = store.getCurrentScenario();
   const useCase = store.getCurrentUseCase();
 
@@ -194,6 +211,12 @@ function scenarioEnded(options, status) {
         pendingScenarios: useCase.pendingScenarios + 1
       });
       break;
+    case DISABLED:
+      store.updateCurrentUseCase({
+        disabledScenarios: useCase.disabledScenarios + 1
+      });
+      // do not further report disabled scenarios (--> return!)
+      return;
     default:
       throw new Error(`Unknown status ${status}`);
   }
