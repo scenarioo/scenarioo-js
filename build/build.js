e5269ac52532fd959f57a2f38a4536b597edc72b
adiherzog <adrian.herzog@gmail.com>
2019-02-18 23:26:17 +0100

    Fix build so that validationSchemas are added again

--- build/build.js
+++ build/build.js
@@ -3,17 +3,13 @@ const
   babel = require('gulp-babel'),
   del = require('del');
 
-gulp.task('cleanLib', () => del('lib/*'));
+gulp.task('clean', () => del('lib/*'));
 
-gulp.task('clean', gulp.series(['cleanLib']));
+gulp.task('copyTypings', () => gulp.src('src/**/*.d.ts').pipe(gulp.dest('lib')));
 
-gulp.task('copyTypings', gulp.series(['cleanLib'], () => gulp.src('src/**/*.d.ts')
-  .pipe(gulp.dest('lib'))));
+gulp.task('copyJsons', () => gulp.src('src/**/*.json').pipe(gulp.dest('lib')));
 
-gulp.task('copyJsons', gulp.series(['cleanLib'], () => gulp.src('src/**/*.json')
-  .pipe(gulp.dest('lib'))));
-
-gulp.task('build', gulp.series(['copyJsons', 'copyTypings', 'lint', 'test'], () => gulp.src('src/**/*.js')
+gulp.task('build', gulp.series(['clean', 'copyJsons', 'copyTypings', 'lint', 'test'], () => gulp.src('src/**/*.js')
   .pipe(babel({
     presets: ['@babel/env']
   }))
